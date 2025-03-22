3f600990bf6ca7aaf226c27a93dfd8263da18cde
Flavia Costa <164424642+Flavia-crfaria@users.noreply.github.com>
2025-03-22 20:21:44 -0300

    API conectada (implementada no android studio) #18

--- src/Entrega 1/Frontend/UberGirls/app/src/main/java/br/com/fecapccp/ubergirls/MainActivity2.java
+++ src/Entrega 1/Frontend/UberGirls/app/src/main/java/br/com/fecapccp/ubergirls/MainActivity2.java
@@ -1,6 +1,7 @@
 package br.com.fecapccp.ubergirls;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.ubergirls_main);
+
+        // Mantém o padding do sistema (EdgeToEdge)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inicializa o mapa
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        } else {
+            Log.e("MAP_DEBUG", "Erro: fragmento do mapa não encontrado!");
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Log para confirmar que o mapa foi carregado
+        Log.d("MAP_DEBUG", "Mapa carregado com sucesso!");
+
+        // Centraliza o mapa em São Paulo
+        LatLng saoPaulo = new LatLng(-23.5505, -46.6333);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(saoPaulo, 12));
     }
 }
