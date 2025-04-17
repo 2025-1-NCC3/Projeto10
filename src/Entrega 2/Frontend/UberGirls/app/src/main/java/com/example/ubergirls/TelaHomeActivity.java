721d11219e564a724318e1d184ff9ed10397f72f
DevGuiMuniz <guigugui16@gmail.com>
2025-04-17 17:24:39 -0300

    add java dos xml #41

---
+++ src/Entrega 2/Frontend/UberGirls/app/src/main/java/com/example/ubergirls/TelaHomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.ubergirls;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.fecapccp.ubergirls.R;
+
+public class TelaHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_home);
+    }
+}
