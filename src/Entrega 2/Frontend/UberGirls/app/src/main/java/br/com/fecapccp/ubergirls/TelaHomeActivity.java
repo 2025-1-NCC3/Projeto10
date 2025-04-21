1661d35a87cada4684177e0f805cdd3069c0102d
Lucas Moreira <lucas.godoy@edu.fecap.br>
2025-04-21 03:16:22 -0300

    Add funcionalidade UberGirls #43
    Adicionei a funcionalidade em Java na tela Uber Girls, Mudei o código do mapa pra fixar de acordo com a localização de cada usuário

--- src/Entrega 2/Frontend/UberGirls/app/src/main/java/br/com/fecapccp/ubergirls/TelaHomeActivity.java
+++ src/Entrega 2/Frontend/UberGirls/app/src/main/java/br/com/fecapccp/ubergirls/TelaHomeActivity.java
@@ -1,15 +1,13 @@
-package com.example.ubergirls;
+package br.com.fecapccp.ubergirls;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
-import br.com.fecapccp.ubergirls.R;
 
 public class TelaHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_home);
+        setContentView(R.layout.telahome_main);
     }
 }
